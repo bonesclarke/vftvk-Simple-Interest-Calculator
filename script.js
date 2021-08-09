58a2aaafedfc0225829382a8a68f738a0e4830e1
bonesclarke <59703067+bonesclarke@users.noreply.github.com>
2021-08-09 06:51:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
     
     //Check if principal field is empty or has a negative value
     if(principal < 0){
@@ -20,6 +15,14 @@ function compute()
     }
     //If all is well return true.
     return true;
+    
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+    
+
 }
 function updateRate() 
 {
